ad220f421bc033e1316ab5e1d6328e452ad0330b
Volkov Daniil <volkov.study@mail.ru>
2023-02-02 19:54:44 +0300

    Add LinkedList#pushBack(...)

--- src/linked-list/LinkedList.h
+++ src/linked-list/LinkedList.h
@@ -13,7 +13,42 @@ class LinkedList {
 private:
     Node<T>* _head;
     Node<T>* _tail;
+public:
+    void pushBack(T data);
 };
 
+template<class T>
+void LinkedList<T>::pushBack(T data) {
+    bool isEmpty = _head == nullptr;
+    if (isEmpty) {
+        Node<T>* node = new Node<T>;
+        node->value = data;
+
+        _head = node;
+        _tail = _head;
+
+        return;
+    }
+
+    bool hasOnlyOneElement = _head == _tail;
+    if (hasOnlyOneElement) {
+        Node<T>* node = new Node<T>;
+        node->value = data;
+        node->previous = _head;
+
+        _head->next = node;
+        _tail = node;
+
+        return;
+    }
+
+    Node<T>* node = new Node<T>;
+    node->value = data;
+    node->previous = _tail;
+
+    _tail->next = node;
+
+    _tail = node;
+}
 
 #endif
