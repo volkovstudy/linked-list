384fe3b7cf73648fd03124f79e41bfb2b2930b10
Volkov Daniil <volkov.study@mail.ru>
2023-02-05 17:30:09 +0300

    Add test for LinkedList#isEmpty()

--- tests/linked-list-tests/LinkedListTests.cpp
+++ tests/linked-list-tests/LinkedListTests.cpp
@@ -3,6 +3,20 @@
 
 #include "linked-list/LinkedList.h"
 
+void shouldReturnFalseWhenListIsNotEmpty();
+
 int main() {
+    shouldReturnFalseWhenListIsNotEmpty();
+
     return 0;
+}
+
+void shouldReturnFalseWhenListIsNotEmpty() {
+    LinkedList<int> list;
+
+    list.pushBack(1);
+    list.pushBack(2);
+    list.pushBack(3);
+
+    assert(list.isEmpty() == false);
 }
