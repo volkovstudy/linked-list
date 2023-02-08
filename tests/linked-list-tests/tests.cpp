5fe61acb7a8d8cc21027a0ed950ddbec9e6077bd
Volkov Daniil <volkov.study@mail.ru>
2023-02-08 21:28:08 +0300

    Add givenListWithItems(...) method in linked-list-tests/tests.cpp

--- tests/linked-list-tests/tests.cpp
+++ tests/linked-list-tests/tests.cpp
@@ -1,1 +1,18 @@
 #include "gtest/gtest.h"
+
+#include <list>
+
+#include "linked-list/LinkedList.h"
+
+using namespace std;
+
+template<class T>
+LinkedList<T>& givenListWithItems(list<T> data) {
+    LinkedList<T>* list = new LinkedList<T>{};
+
+    for (const T value: data) {
+        list->pushBack(value);
+    }
+
+    return *list;
+}
